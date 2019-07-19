f799d14bf2b466576259a2a46b0db859c2d91592
Nikita Nikitin <nioktn@gmail.com>
2019-07-19 15:40:34 +0300

    Implemented Create(POST) feature

---
+++ Vega/Controllers/Resources/VehicleResource.cs
@@ -0,0 +1,24 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Vega.Models;
+
+namespace Vega.Controllers.Resources
+{
+    public class VehicleResource
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public bool IsRegistered { get; set; }
+        public IList<int> Features { get; set; }
+        [Required]
+        public ContactResource Contact { get; set; }
+        public VehicleResource()
+        {
+            Features = new List<int>();
+        }
+
+    }
+}
---
+++ Vega/Controllers/VehiclesController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Vega.Controllers.Resources;
+using Vega.Models;
+using Vega.Persistence;
+
+namespace Vega.Controllers
+{
+    [Route("/api/vehicles")]
+    public class VehiclesController : Controller
+    {
+        private readonly IMapper mapper;
+        private readonly VegaDbContext context;
+
+        public VehiclesController(IMapper mapper, VegaDbContext context)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        //[HttpPost("/api/vehicles")]
+        [HttpPost]
+        public async Task<IActionResult> CreateVehicle([FromBody]VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var model = await context.Models.FindAsync(vehicleResource.ModelId);
+            if (model == null)
+            {
+                ModelState.AddModelError("ModelId", "Invalid model Id.");
+                return BadRequest(ModelState);
+            }
+                        
+            foreach (var feature in vehicleResource.Features)
+            {
+                if (await context.Features.FindAsync(feature) == null)
+                {
+                    ModelState.AddModelError("ModelId", $"Given featureId ({feature}) doesn't exist.");
+                    return BadRequest(ModelState);
+                }
+            }
+
+            if (!vehicleResource.Features.Any())
+            {
+                ModelState.AddModelError("Features", "The vehicle must have 1 or more features.");
+                return BadRequest(ModelState);
+            }
+            
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            vehicle.LastUpdate = DateTime.Now;
+
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
+    }
+}
